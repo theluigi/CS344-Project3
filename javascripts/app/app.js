69e47aa3b6ab700f8c8d69fc8509e3089f91086e
TheLuigi <jruggier@unca.edu>
2013-03-21 15:52:25 -0400

    Loads tasks from all.json, gives each its own class

--- javascripts/app/app.js
+++ javascripts/app/app.js
@@ -1,5 +1,7 @@
 var main = function () {
 
+	var categoriesList = "" , taskCounter = 0; 
+	
     var setUpClickHandler = function (anchor) {
         anchor.click(function () {
             var target = $(this).attr("href");
@@ -15,9 +17,28 @@ var main = function () {
 	var loadJSON = function() {
 		$.getJSON("all.json", function (myTodos) {
 			myTodos.forEach(function (myTodo) {
-				myTodo.categories.forEach(console.log(category));
-		}); 
+				myTodo.categories.forEach(function(category) {
+					category = " " + category;
+					categoriesList = categoriesList + category; //add categories from all.json
+				});
+				var desc = myTodo.description;
+				addTodoData(desc, categoriesList); 
+			}); 
+		});
+	
 	}; 
+
+	//todo function
+	var addTodoData = function(text, cats) {
+		taskCounter++; 
+		  $("#All").append("<div id = task" + taskCounter + ">");
+		  $("#task" + taskCounter).html("<p>" + text + " /// " + cats + "</p>");
+		
+	}; 
+	
+	
+	
+	
 
 	var initialise = function () {
 	
